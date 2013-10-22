726f9803edfb55d8cfec1d1e3c2372f8fbe9150e
Dmitry Marakasov <amdmi3@amdmi3.ru>
2013-10-23 00:48:47 +0400

    Stricten pkg checks

--- ports-mgmt/pkg-strict/files/patch-libpkg__pkg.c
+++ ports-mgmt/pkg-strict/files/patch-libpkg__pkg.c
@@ -1,5 +1,105 @@
---- libpkg/pkg.c.orig	2013-07-06 12:48:19.000000000 +0200
-+++ libpkg/pkg.c	2013-10-05 19:45:16.675062417 +0200
+--- libpkg/pkg.c.orig	2013-07-06 14:48:19.000000000 +0400
++++ libpkg/pkg.c	2013-10-23 00:44:11.274197440 +0400
+@@ -475,8 +475,8 @@
+ 
+ 	HASH_FIND_STR(pkg->licenses, __DECONST(char *, name), l);
+ 	if (l != NULL) {
+-		pkg_emit_error("duplicate license listing: %s, ignoring", name);
+-		return (EPKG_OK);
++		pkg_emit_error("duplicate license listing: %s, fatal", name);
++		return (EPKG_FATAL);
+ 	}
+ 
+ 	pkg_license_new(&l);
+@@ -498,8 +498,8 @@
+ 
+ 	HASH_FIND_STR(pkg->users, __DECONST(char *, name), u);
+ 	if (u != NULL) {
+-		pkg_emit_error("duplicate user listing: %s, ignoring", name);
+-		return (EPKG_OK);
++		pkg_emit_error("duplicate user listing: %s, fatal", name);
++		return (EPKG_FATAL);
+ 	}
+ 
+ 	pkg_user_new(&u);
+@@ -532,8 +532,8 @@
+ 
+ 	HASH_FIND_STR(pkg->groups, __DECONST(char *, name), g);
+ 	if (g != NULL) {
+-		pkg_emit_error("duplicate group listing: %s, ignoring", name);
+-		return (EPKG_OK);
++		pkg_emit_error("duplicate group listing: %s, fatal", name);
++		return (EPKG_FATAL);
+ 	}
+ 
+ 	pkg_group_new(&g);
+@@ -569,9 +569,9 @@
+ 	HASH_FIND_STR(pkg->deps, __DECONST(char *, origin), d);
+ 	if (d != NULL) {
+ 		pkg_get(pkg, PKG_NAME, &n1, PKG_VERSION, &v1);
+-		pkg_emit_error("%s-%s: duplicate dependency listing: %s-%s, ignoring",
++		pkg_emit_error("%s-%s: duplicate dependency listing: %s-%s, fatal",
+ 		    n1, v1, name, version);
+-		return (EPKG_OK);
++		return (EPKG_FATAL);
+ 	}
+ 
+ 	pkg_dep_new(&d);
+@@ -627,8 +627,8 @@
+ 	if (check_duplicates) {
+ 		HASH_FIND_STR(pkg->files, __DECONST(char *, path), f);
+ 		if (f != NULL) {
+-			pkg_emit_error("duplicate file listing: %s, ignoring", pkg_file_path(f));
+-			return (EPKG_OK);
++			pkg_emit_error("duplicate file listing: %s, fatal", pkg_file_path(f));
++			return (EPKG_FATAL);
+ 		}
+ 	}
+ 
+@@ -662,8 +662,8 @@
+ 
+ 	HASH_FIND_STR(pkg->categories, __DECONST(char *, name), c);
+ 	if (c != NULL) {
+-		pkg_emit_error("duplicate category listing: %s, ignoring", name);
+-		return (EPKG_OK);
++		pkg_emit_error("duplicate category listing: %s, fatal", name);
++		return (EPKG_FATAL);
+ 	}
+ 
+ 	pkg_category_new(&c);
+@@ -693,8 +693,8 @@
+ 	if (check_duplicates) {
+ 		HASH_FIND_STR(pkg->dirs, __DECONST(char *, path), d);
+ 		if (d != NULL) {
+-			pkg_emit_error("duplicate directory listing: %s, ignoring", path);
+-			return (EPKG_OK);
++			pkg_emit_error("duplicate directory listing: %s, fatal", path);
++			return (EPKG_FATAL);
+ 		}
+ 	}
+ 
+@@ -816,8 +816,8 @@
+ 
+ 	HASH_FIND_STR(pkg->options, __DECONST(char *, key), o);
+ 	if (o != NULL) {
+-		pkg_emit_error("duplicate options listing: %s, ignoring", key);
+-		return (EPKG_OK);
++		pkg_emit_error("duplicate options listing: %s, fatal", key);
++		return (EPKG_FATAL);
+ 	}
+ 	pkg_option_new(&o);
+ 
+@@ -892,8 +892,8 @@
+ 	HASH_FIND_STR(pkg->annotations, __DECONST(char *, tag), an);
+ 	if (an != NULL) {
+ 		pkg_emit_error("duplicate annotation tag: %s value: %s,"
+-			       " ignoring", tag, value);
+-		return (EPKG_OK);
++			       " fatal", tag, value);
++		return (EPKG_FATAL);
+ 	}
+ 	an = NULL;
+ 	pkg_annotation_new(&an);
 @@ -1181,6 +1181,20 @@
  	struct pkg_dir *dir = NULL;
  	char spath[MAXPATHLEN + 1];
